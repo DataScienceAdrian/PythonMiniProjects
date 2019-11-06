c48c956853528888092bc5807d7997e308742726
Adrian Marcinczyk <amarc@softserveinc.com>
2019-11-06 09:18:50 +0100

    Task 2 sucessfully done

--- PythonAcademyTasks/RequestMethod.py
+++ PythonAcademyTasks/RequestMethod.py
@@ -1,12 +1,11 @@
-import re
-
 import requests
 import bs4 as bs
 
 response = requests.get("https://en.wikipedia.org/wiki/Rutland_Boughton").text
 response2 = requests.get("https://www.python.org/dev/peps/").text
 
-#Zad 1
+
+# Zad 1
 def main_func():
     connection_check()
     data_scrap()
@@ -40,42 +39,37 @@ def data_scrap():
         print("https:", url)
 
 
-#main_func()
-
-#Zad2
+# Zad2
 
 def data_scrap2():
     soup = bs.BeautifulSoup(response2, 'lxml')
     td_tags = soup.find_all('td')
 
     pep_number1 = td_tags[8].text
-    print(pep_number1)
-
     pep_title1 = td_tags[9].text
-    print(pep_title1)
-
     pep_authors1 = td_tags[10].text
-    print(pep_authors1)
+    pep_class_type = soup.findAll("a", {"class": "reference external"})
+    pep_url1 = pep_class_type[1].get('href')
 
-    links = []
+    pep_number2 = td_tags[12].text
+    pep_title2 = td_tags[13].text
+    pep_authors2 = td_tags[14].text
+    pep_class_type2 = soup.findAll("a", {"class": "reference external"})
+    pep_url2 = pep_class_type2[2].get('href')
 
-    url_tags = soup.find_all('a')
-    #print(url_tags)
-
-    myclasses = soup.findAll("a", {"class": "reference external"})
-    #print(myclasses)
-
-    pep_url = myclasses[1]
-    pep_string = myclasses[1]
-    print(pep_string)
-
-    print(pep_url)
-    #//*[@id="meta-peps-peps-about-peps-or-processes"]/table/tbody/tr[1]/td[2]/a
-
+    print('pep_number:', pep_number1, '\n', 'pep_title:', pep_title1, '\n', 'pep_authors:', pep_authors1, '\n'
+                                                                                                          'pep_url:',
+          pep_url1)
+    print('\n\n')
+    print('pep_number:', pep_number2, '\n', 'pep_title:', pep_title2, '\n', 'pep_authors:', pep_authors2, '\n'
+                                                                                                          'pep_url:',
+          pep_url2)
 
 
 def main_func2():
     connection_check()
     data_scrap2()
 
+
+# main_func()
 main_func2()
